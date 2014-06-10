1b9385a9515ad4fa70d2ea77bbfe1e4f71f65eb5
Eddie <edaniels@worldwidetechnologygroup.com>
2014-06-10 21:34:07 +0000

    Autoloader - should use the composers vendor autoloader

---
+++ autoload.php
@@ -0,0 +1,12 @@
+<?php
+// You should use composers autoloader!!! This is just for demo purposes...
+spl_autoload_register(function ($class) {
+    // strip our vendor
+    $class = str_replace("stratease\ImageBuilder\\", "", $class);
+    // break into paths
+    $classPath = __DIR__.'/src/' .str_replace("\\", "/", $class).".php";
+
+    if(file_exists($classPath)) {
+        require_once( $classPath );
+    }
+});
